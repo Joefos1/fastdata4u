d1aef76d651763f57978b701566bd24f1d8477b4
Joefos1 <joefositservices@gmail.com>
2026-08-19 10:11:37 +0000

    Configure Vite with React and path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,12 @@
+import {definConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path"; 
 
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(_dirname,  "./src"),
+    },
+  },
+});
